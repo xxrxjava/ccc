1c1d8cf50d6f492c3adf6900c6f631e0899b9b80
xxrxjava <87923620+xxrxjava@users.noreply.github.com>
2021-07-31 06:45:06 -0700

    Update lambda.kt

--- lambda.kt
+++ lambda.kt
@@ -1,3 +1,32 @@
+   fun main() {
+        showOnBoard("卫生纸"){goodsName:String,hour:Int ->
+           val currentYear = 2021
+            "${currentYear}年,双11${goodsName}促销倒计时：$hour 小时"
+       }
+    }
+        
+        
+  private fun showOnBoard(goodsName:String,getdiscountwords:(String,Int)->String){
+        val hour:Int = (1..24).shuffled().last()
+        println(getdiscountwords(goodsName,hour))
+    }
+}
+
+//    fun main() {
+//        val getdiscountwords ={goodsName:String,hour:Int ->
+//            val currentYear = 2021
+//            "${currentYear}年,双11${goodsName}促销倒计时：$hour 小时"
+//        }
+//        showOnBoard("卫生纸",getdiscountwords)
+//    }
+//    fun showOnBoard(goodsName:String,getdiscountwords:(String,Int)->String){
+//        val hour:Int = (1..24).shuffled().last()
+//        println(getdiscountwords(goodsName,hour))
+//    }
+
+
+/////////////////////////////////////////
+
 fun main() {
 
     val total:Int = "hello".count()
