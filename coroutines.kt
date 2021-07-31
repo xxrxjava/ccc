4cb0fd57dfbf80a486b81f61cdb2c231427d280a
xxrxjava <87923620+xxrxjava@users.noreply.github.com>
2021-07-31 05:23:28 -0700

    Create coroutines.kt

---
+++ coroutines.kt
@@ -0,0 +1,27 @@
+fun main() {
+    GlobalScope.launch {
+        val e = async { SuspendFunctions.add(1, 3) }
+        val f = async { SuspendFunctions.sadd(4, 6) }
+        print(e.await() + f.await())
+    }
+    readLine()
+}
+
+object SuspendFunctions {
+    suspend fun add(a: Int, b: Int): Int {
+        return withContext(Dispatchers.Default) {
+            delay(2000)
+            a + b
+        }
+    }
+
+    suspend fun sadd(a: Int, b: Int): Int {
+        return withContext(Dispatchers.Default) {
+            delay(4000)
+            a + b
+        }
+    }
+}
+
+    implementation "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1"
+    implementation 'org.jetbrains.kotlinx:kotlinx-coroutines-android:1.0.1'
